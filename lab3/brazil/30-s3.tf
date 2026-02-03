f3f325735fa8518d954bdd567e70306d71a0cff1
Larry G Harris <32481510+Nightwolf197676@users.noreply.github.com>
2026-02-03 16:12:50 -0500

    Delete lab3/brazil directory

--- lab3/brazil/30-s3.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "brazil-medical-s3-backend-891377135193"          # your existing bucket
-    key            = "gru/ap-northeast-1/terraform.tfstate"
-    region         = "sa-east-1"                        # bucket region
-  #  encrypt        = true
-    #dynamodb_table = "terraform-locks"                  # optional but strongly recommended for locking
-  }
-}
